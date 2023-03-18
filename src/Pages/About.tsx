4ea42147c607c951b4e9eb38da3fdfa64a94f7e5
Amrit Bastakoti <amritbastakoti618@gmail.com>
2023-03-18 23:12:57 +0200

    about page modified

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,31 +1,62 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/About.css';
+import { Card, CardContent, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  card: {
+    marginBottom: '16px',
+    boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.3)',
+  },
+  intro: {
+    paddingBottom: '16px',
+  },
+  skills: {
+    paddingTop: '16px',
+    paddingBottom: '16px',
+  },
+  hireMe: {
+    marginTop: '32px',
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: '100vh',
+  },
+});
 
 const About: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <div className="about-container">
+    <div className={classes.container}>
       <h1>About Me</h1>
-      <div className="intro">
-        <p>
-          Hi there! I'm a enthusiastic coder with a passion for JavaScript and
-          cloud services.
-        </p>
-      </div>
-      <div className="skills">
-        <h2>Skills and Experience</h2>
-        <ul>
-          <li>Strong proficiency in JavaScript, HTML, and CSS</li>
-          <li>Experience with cloud services such as AWS and Azure</li>
-          <li>Hands-on experience with Node.js, React, and Redux</li>
-          <li>Excellent problem-solving and analytical skills</li>
-          <li>Strong communication and teamwork skills</li>
-        </ul>
-      </div>
-      <div className="hire-me">
-        <Link to="/contact">
-          <button>Hire Me</button>
-        </Link>
+      <Card className={classes.card}>
+        <CardContent className={classes.intro}>
+          <h2>Introduction</h2>
+          <p>
+            Hi there! I'm an enthusiastic coder with a passion for JavaScript
+            and cloud services.
+          </p>
+        </CardContent>
+      </Card>
+      <Card className={classes.card}>
+        <CardContent className={classes.skills}>
+          <h2>Skills and Experience</h2>
+          <ul>
+            <li>Strong proficiency in JavaScript, HTML, and CSS</li>
+            <li>Experience with cloud services such as AWS and Azure</li>
+            <li>Hands-on experience with Node.js, React, and Redux</li>
+            <li>Excellent problem-solving and analytical skills</li>
+            <li>Strong communication and teamwork skills</li>
+          </ul>
+        </CardContent>
+      </Card>
+      <div className={classes.hireMe}>
+        <Button variant="contained" color="primary">
+          Hire Me
+        </Button>
       </div>
     </div>
   );
