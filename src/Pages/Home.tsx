2f1441788a66f81a0ac6ce0850023699102aa693
Amrit Bastakoti <amritbastakoti618@gmail.com>
2023-04-10 15:32:12 +0300

    home page modified

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,29 +2,27 @@ import React from 'react';
 import '../styles/HomePage.css';
 import profileImage from '../images/profile-image.png';
 
-
 function Home() {
   return (
     <div className="home-page">
       <div className="profile-section">
-        <img src={profileImage} alt="Profile" />
-        <h1>Amrit Bastakoti</h1>
-        <p>Web Developer | Cloud Engineer</p>
-        <p></p>
+        <img src={profileImage} alt="Profile" className="profile-image" />
+        <h1 className="profile-name">Amrit Bastakoti</h1>
+        <p className="profile-title">Web Developer | Cloud Engineer</p>
         <button className="hire-button">Hire me</button>
       </div>
       <div className="cards-section">
         <div className="card">
-          <h2>Passionate about Programming</h2>
-          <p>
+          <h2 className="card-title">Passionate about Programming</h2>
+          <p className="card-description">
             I am passionate about programming and always eager to learn new
             technologies. I believe that my passion for coding allows me to
             produce high-quality work.
           </p>
         </div>
         <div className="card">
-          <h2>Expert in Cloud Services</h2>
-          <p>
+          <h2 className="card-title">Expert in Cloud Services</h2>
+          <p className="card-description">
             I have hands-on experience with various cloud services like AWS,
             Azure, and GCP. I can help you optimize your cloud infrastructure
             and reduce your costs.
@@ -32,9 +30,11 @@ function Home() {
         </div>
 
         <div className="big-card">
-          <h1> Hey this is a big card</h1>
-          <h2>Time management</h2>
-          <p>The time can be managed within your desired.</p>
+          <h1 className="big-card-title">Hey, this is a big card</h1>
+          <h2 className="big-card-subtitle">Time management</h2>
+          <p className="big-card-description">
+            Time can be managed within your desired timeframe.
+          </p>
         </div>
       </div>
     </div>
