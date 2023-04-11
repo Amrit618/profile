692046e37f02e699093de375e334378c3a8b10ae
Amrit Bastakoti <amritbastakoti618@gmail.com>
2023-04-11 22:14:07 +0300

    newsletter updated

--- src/Pages/Newsletter.tsx
+++ src/Pages/Newsletter.tsx
@@ -1,14 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Newsletter = () => {
-  const newsletter = {}
+type FormState = {
+  email: string;
+};
+
+const NewsletterForm: React.FC = () => {
+  const [formState, setFormState] = useState<FormState>({
+    email: '',
+  });
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormState({
+      ...formState,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Add logic to handle form submission, e.g. API call or data validation
+    console.log('Form submitted with email:', formState.email);
+    setFormState({
+      email: '',
+    });
+  };
+
   return (
-    <>
-    <div>Newsletter</div>
-    <input type='text' />
-
-    </>
+    <form onSubmit={handleSubmit}>
+      <h2>Subscribe to our Newsletter</h2>
+      <input
+        type="email"
+        name="email"
+        placeholder="Enter your email"
+        value={formState.email}
+        onChange={handleInputChange}
+        required
+      />
+      <button type="submit">Subscribe</button>
+    </form>
   );
-}
+};
 
-export default Newsletter
+export default NewsletterForm;
--- src/cvData.ts
+++ src/cvData.ts
@@ -41,3 +41,4 @@ const cvData = {
 };
 
 export default cvData;
+
